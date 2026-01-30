04f5a042533e9b1e04e9f260fb84e2e5b51e12ad
Julie Thornton <juliet@ksu.edu>
2026-01-30 14:37:45 -0600

    notes before feb 2 and 3

---
+++ tuesThurs_sectionB/week3_feb2-6/tuesFeb3/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a postive number: ");
+        int num = s.nextInt();
+
+        //print the factorial of num, like:
+        //5! = 5*4*3*2*1 = 120
+        //how to add input validation?
+    }
+}
