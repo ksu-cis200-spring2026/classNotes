aa9c5820169c7e97319893e437c89423a2e587a4
Julie <juliet@ksu.edu>
2026-02-02 15:03:01 -0600

    after mon feb 2

--- monWed_sectionA/week3_feb2-6/monFeb2/Factorial.java
+++ monWed_sectionA/week3_feb2-6/monFeb2/Factorial.java
@@ -7,6 +7,23 @@ public class Factorial {
         System.out.print("Enter a postive number: ");
         int num = s.nextInt();
 
+        if (num >= 1) {
+            System.out.printf("%d! = ", num);
+
+            //will loop num times
+            int product = 1;
+            for (int i = num; i >= 1; i--) {
+                product *= i;
+                System.out.printf("%d", i);
+                if (i > 1) System.out.print("*");
+            }
+            System.out.printf(" = %d%n", product);
+        }
+        else {
+            System.out.println("Error: input must be positive");
+        }
+        
+
         //print the factorial of num, like:
         //5! = 5*4*3*2*1 = 120
         //how to add input validation?
