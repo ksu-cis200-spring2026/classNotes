52430ee9060a17a2fb4688047248c628dacd0f31
Julie <juliet@ksu.edu>
2026-02-03 16:24:53 -0600

    before feb 4 and 5

---
+++ monWed_sectionA/week3_feb2-6/wedFeb4/Feb4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Feb4 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner();
+
+        System.out.print("Enter a dice value (1-6): ");
+        int val = s.nextInt();
+
+        /*
+            Example: user enters a dice value (1-6)
+            repeatedly roll a die (user presses Enter to roll)
+            until user rolls that value
+
+            Afterwards:
+            - Print how many rolls it took
+            - Print the highest roll
+            - Print "you never rolled a 6", if applicable
+                ("flag" variables)
+        */
+    }
+}
