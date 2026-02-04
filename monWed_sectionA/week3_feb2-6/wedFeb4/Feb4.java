afdd880297b235a397a3d797812962aea3bb297e
Julie <juliet@ksu.edu>
2026-02-04 15:04:05 -0600

    after wed feb 4

--- monWed_sectionA/week3_feb2-6/wedFeb4/Feb4.java
+++ monWed_sectionA/week3_feb2-6/wedFeb4/Feb4.java
@@ -3,20 +3,23 @@ import java.util.*;
 public class Feb4 {
     public static void main(String[] args) {
         Scanner s = new Scanner();
+        Random r = new Random();
 
         System.out.print("Enter a dice value (1-6): ");
         int val = s.nextInt();
+        int dice;
+
+        do {
+            System.out.print("Press Enter to roll.");
+            s.nextLine();
+            dice = r.nextInt(6) + 1;
+            System.out.printf("You rolled %d%n", dice);
+        } while (dice != val);
 
         /*
             Example: user enters a dice value (1-6)
             repeatedly roll a die (user presses Enter to roll)
             until user rolls that value
-
-            Afterwards:
-            - Print how many rolls it took
-            - Print the highest roll
-            - Print "you never rolled a 6", if applicable
-                ("flag" variables)
         */
     }
 }
