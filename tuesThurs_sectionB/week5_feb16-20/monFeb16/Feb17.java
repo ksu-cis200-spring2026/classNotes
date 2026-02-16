eca7246d493f45e2be3ed2c682d2ef88c6e09c1b
Julie <juliet@ksu.edu>
2026-02-16 10:55:26 -0600

    notes before feb 16 and 17

---
+++ tuesThurs_sectionB/week5_feb16-20/monFeb16/Feb17.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Feb17 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        /*
+            Get 6 numbers from the user.
+
+            Print whether any input number is the square of another 
+            input number.
+        */
+    }
+}
