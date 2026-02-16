b6fb66ed6e9cbcfcfccdf5921395e61763095eb9
Julie <juliet@ksu.edu>
2026-02-16 15:10:24 -0600

    after mon feb 16

--- monWed_sectionA/week5_feb16-20/monFeb16/Feb16.java
+++ monWed_sectionA/week5_feb16-20/monFeb16/Feb16.java
@@ -4,11 +4,21 @@ public class Feb16 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        /*
-            Get 6 numbers from the user.
+        int[] vals = new int[6];
+        System.out.print("Enter 6 numbers: ");
+        for (int i = 0; i < vals.length; i++) {
+            vals[i] = s.nextInt();
+        }
 
-            Print whether any input number is the square of another 
-            input number.
+        System.out.println("Reversed:");
+        for (int i = vals.length-1; i >= 0; i--) {
+            System.out.println(vals[i]);
+        }
+
+        /*
+            Get 6 numbers from the user, all in a line
+
+            Print them in reverse
         */
     }
 }
