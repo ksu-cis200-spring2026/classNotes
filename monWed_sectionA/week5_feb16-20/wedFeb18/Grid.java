ef18c5fbcbc0862d5ce2fae2f516f3d86f3a5a10
Julie Thornton <juliet@ksu.edu>
2026-02-18 11:41:04 -0600

    added feb 18 and 19 notes

---
+++ monWed_sectionA/week5_feb16-20/wedFeb18/Grid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Grid {
+    public static void main(String[] args) {
+        /*
+            Create a 7x3 array of doubles.
+
+            Initialize first row to be: 1.0, 1.1, 1.2
+            Initialize the second row to be: 2.0, 2.1, 2.2
+            ...etc.
+
+
+            Then print array
+        */
+    }
+}
