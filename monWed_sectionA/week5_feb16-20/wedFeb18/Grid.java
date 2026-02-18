1960c287b3fcb3d5ed7b19b75caebe1f8d39a6ac
Julie <juliet@ksu.edu>
2026-02-18 15:03:34 -0600

    after wed feb 18

--- monWed_sectionA/week5_feb16-20/wedFeb18/Grid.java
+++ monWed_sectionA/week5_feb16-20/wedFeb18/Grid.java
@@ -2,6 +2,27 @@ import java.util.*;
 
 public class Grid {
     public static void main(String[] args) {
+        double[][] grid = new double[7][3];
+
+        //outer loop through rows
+        for (int i = 0; i < grid.length; i++) {
+            double cur = i+1;
+
+            //inner loop through cols
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = cur;
+                cur += 0.1;
+            }
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            //inner loop through cols
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.printf("%.2f ", grid[i][j]);
+            }
+            System.out.println();
+        }
+
         /*
             Create a 7x3 array of doubles.
 
