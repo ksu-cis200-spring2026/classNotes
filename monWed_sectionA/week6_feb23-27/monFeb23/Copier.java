5d6702d078e0749466f2c4bb8e041e81af047337
Julie <juliet@ksu.edu>
2026-02-23 10:48:45 -0600

    notes before feb 23 and 24

---
+++ monWed_sectionA/week6_feb23-27/monFeb23/Copier.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Copier {
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+
+        //ask user for name of file
+       
+        
+        //create output file named copy_origName
+        //that is a duplicate of original file
+
+    }
+}
