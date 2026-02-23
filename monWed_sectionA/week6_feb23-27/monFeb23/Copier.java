ecc3341172096f0179a73fb4a8140df5efa4f473
Julie <juliet@ksu.edu>
2026-02-23 15:08:08 -0600

    after mon feb 23

--- monWed_sectionA/week6_feb23-27/monFeb23/Copier.java
+++ monWed_sectionA/week6_feb23-27/monFeb23/Copier.java
@@ -6,7 +6,20 @@ public class Copier {
         Scanner s = new Scanner(System.in);
 
         //ask user for name of file
+        System.out.print("Enter name of input file: ");
+        String input = s.nextLine();
+
+        //connect to input file
+        Scanner inFile = new Scanner(new File(input));
+        PrintWriter outFile = new PrintWriter("copy_"+input);
        
+        while(inFile.hasNext()) {
+            String line = inFile.nextLine();
+            outFile.println(line);
+        }
+
+        inFile.close();
+        outFile.close();
         
         //create output file named copy_origName
         //that is a duplicate of original file
