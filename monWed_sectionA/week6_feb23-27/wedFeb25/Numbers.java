90697c38c3d50b14dc66035fc30af02448bea59b
Julie <juliet@ksu.edu>
2026-02-24 16:06:42 -0600

    notes before feb 25 and 26

---
+++ monWed_sectionA/week6_feb23-27/wedFeb25/Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a list of numbers separated by commas: ");
+        String line = s.nextLine();
+
+        //like: 12,4,-3.2,5
+
+        //how to print the sum of the numbers WITHOUT split?
+
+    }
+}
