84a7c84834625c2e30be0d36fe59d94cfffb92ff
Julie <juliet@ksu.edu>
2026-02-24 13:44:16 -0600

    after tues feb 24

--- tuesThurs_sectionB/week6_feb23-27/tuesFeb24/Copier.java
+++ tuesThurs_sectionB/week6_feb23-27/tuesFeb24/Copier.java
@@ -2,14 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Copier {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
 
         //ask user for name of file
         System.out.print("Enter name of input file: ");
         String input = s.nextLine();
        
-        
+        Scanner inFile = new Scanner(new File(input));
+        PrintWriter outFile = new PrintWriter("copy_" + input);
+
+        while (inFile.hasNext()) {
+            String line = inFile.nextLine();
+            outFile.println(line);
+        }
+
+        inFile.close();
+        outFile.close();
+
         //create output file named copy_origName
         //that is a duplicate of original file
 
