9345de3c654c657fe3c185fdeb02c17e9ab7e575
Julie <juliet@ksu.edu>
2026-02-25 15:09:02 -0600

    after wed feb 25

--- monWed_sectionA/week6_feb23-27/wedFeb25/Numbers.java
+++ monWed_sectionA/week6_feb23-27/wedFeb25/Numbers.java
@@ -7,6 +7,28 @@ public class Numbers {
         System.out.print("Enter a list of numbers separated by commas: ");
         String line = s.nextLine();
 
+        double sum = 0;
+        int index = 0;
+        boolean done = false;
+        do {
+            int comma = line.indexOf(",", index);
+
+            String piece;
+            if (comma == -1) {
+                piece = line.substring(index);
+                done = true;
+            }
+            else {
+                piece = line.substring(index,comma);
+            }
+
+            index = comma+1;
+
+            sum += Double.parseDouble(piece);
+        } while (!done);
+
+        System.out.println(sum);
+
         //like: 12,4,-3.2,5
 
         //how to print the sum of the numbers WITHOUT split?
