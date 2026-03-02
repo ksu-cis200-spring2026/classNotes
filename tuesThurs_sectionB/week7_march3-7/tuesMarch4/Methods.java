f4e977428426b5b74d7f6493d3576fe4c1838284
Julie <juliet@ksu.edu>
2026-03-02 15:10:40 -0600

    notes before tues march 4

---
+++ tuesThurs_sectionB/week7_march3-7/tuesMarch4/Methods.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Methods {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int[] arr = {-3,5,-2,7,17,5};
+
+        //update each array element to be its own absolute value
+
+
+
+        System.out.print("Enter a word: ");
+        String word = s.nextLine();
+
+        //print something like: madam is a palindrome
+        //or loop is not a palindrome
+
+
+        
+        int[] another = {1,4,17,25,36,37,45};
+        System.out.print("Enter a number to search for: ");
+        int num = s.nextInt();
+
+        //print something like: 17 is at index 2
+        //or 5 is not found
+
+    }
+
+    //write a method to compute the absolute value
+
+
+    //write a method to return whether a String 
+    //is a palindrome
+
+
+    //write a method to return position of 
+    //an element in an array (of integers)
+}
