8cfb21a9c2b865e87c96cfe5cdfcc2180c14a6e3
Julie <juliet@ksu.edu>
2026-03-03 13:35:52 -0600

    after tues march 4

--- tuesThurs_sectionB/week7_march3-7/tuesMarch4/Methods.java
+++ tuesThurs_sectionB/week7_march3-7/tuesMarch4/Methods.java
@@ -7,7 +7,9 @@ public class Methods {
         int[] arr = {-3,5,-2,7,17,5};
 
         //update each array element to be its own absolute value
-
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = absVal(arr[i]);
+        }
 
 
         System.out.print("Enter a word: ");
@@ -27,13 +29,53 @@ public class Methods {
 
     }
 
-    //write a method to compute the absolute value
+    //write a method to compute the absolute value (int)
+    public static int absVal(int val) {
+        //-4 should be 4
+        //4 should be 4
 
+        if (val < 0) {
+            return val*-1;
+        }
+        else {
+            return val;
+        }
+    }
 
     //write a method to return whether a String 
     //is a palindrome
+    //spelled same forwards and backwards racecar
+    public static boolean palindrome(String word) {
+        //create StringBuilder
+        //loop through word backwards
+            //append each letter on the StringBuilder
+        //see if StringBuilder equals word
+
+        int front = 0;
+        int back = word.length()-1;
+
+        while (front < back) {
+            if (word.charAt(front) != word.charAt(back)) {
+                return false;
+            }
+
+            front++;
+            back--;
+        }
+
+        return true;
+    }
 
 
     //write a method to return position of 
     //an element in an array (of integers)
+    //{3,2,4}, looking for 4, return 2
+    public static int indexOf(int[] nums, int val) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
