5841684cb32b84b7feafef4aced0610402aea756
Julie <juliet@ksu.edu>
2026-03-10 13:40:41 -0500

    after tues march 10

--- tuesThurs_sectionB/week8_march9-13/tuesMarch10/Methods.java
+++ tuesThurs_sectionB/week8_march9-13/tuesMarch10/Methods.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class Methods {
     public static void main(String[] args) {
+        if (args.length == 2) {
+            char letter = args[0].charAt(0);
+            int num = Integer.parseInt(args[1]);
+            num++;
+            System.out.println(letter + " " + num);
+        }
+        else {
+            System.out.println("Run as: java Methods word number");
+        }
+
         //command-line args: word number
         //java Methods apple 15
 
@@ -10,4 +20,4 @@ public class Methods {
 
         //print error if one or both not provided
     }
-}
+};
