e2345435f504941f54edfdad739b80aa3a9f4141
Julie <juliet@ksu.edu>
2026-03-30 15:16:04 -0500

    after mon march 30

--- monWed_sectionA/week10_march30-april3/monMarch30/Week10.java
+++ monWed_sectionA/week10_march30-april3/monMarch30/Week10.java
@@ -2,8 +2,27 @@ import java.util.*;
 
 public class Week10 {
     public static void main(String[] args) {
+        //create a ConsoleOps object
+        ConsoleOps c = new ConsoleOps();
 
         //get 5 numbers from user, print sum
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = 0;
+
+            //calling this method led to an exception
+            try {
+                num = c.getInteger();
+            }
+            catch (NumberFormatException ex) {
+                c.printError();
+                i--;
+            }
+
+            sum += num;
+        }
+
+        c.printInteger(sum, "The sum is");
 
         //what could crash?
 
--- monWed_sectionA/week10_march30-april3/monMarch30/lab16_start/Cipher.java
+++ monWed_sectionA/week10_march30-april3/monMarch30/lab16_start/Cipher.java
@@ -2,3 +2,35 @@
 //constructor
 //encrypt method (takes message, returns ciphertext)
 //decrypt method (takes ciphertext, returns message)
+
+public class Cipher {
+    private int key;
+
+    public Cipher(int k) {
+        key = k;
+    }
+
+    public String encrypt(String str) {
+        StringBuilder build = new StringBuilder();
+
+        //loop through str
+            char letter = str.charAt(i);
+
+            //is this character uppercase?
+            if (letter >= 'A' && letter <= 'z') {
+                letter+=key;
+
+                if (letter > 'Z') {
+                    letter-=26;
+                }
+            }
+
+            //same thing for lowercase
+
+            build.append(letter);
+
+        return build.toString();
+    }
+
+    //decrypt will be similar
+}
