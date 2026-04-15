b751ebb8a1163fcbe7187617bc213d095540bc44
Julie <juliet@ksu.edu>
2026-04-15 15:11:16 -0500

    before thurs april 16

---
+++ tuesThurs_sectionB/week12_april13-17/thursApril16/April16.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class April16 {
+    public static void main(String[] args) {
+        //create a TreeSet of type Person
+        //add a few
+
+        
+        //print everything
+    }
+}
