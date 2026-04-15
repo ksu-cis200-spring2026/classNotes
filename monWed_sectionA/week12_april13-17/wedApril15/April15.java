58b547428e58b6c168a43efb2defc913c874b2e9
Julie <juliet@ksu.edu>
2026-04-15 15:06:13 -0500

    after mon april 15

--- monWed_sectionA/week12_april13-17/wedApril15/April15.java
+++ monWed_sectionA/week12_april13-17/wedApril15/April15.java
@@ -7,6 +7,17 @@ public class April15 {
         //create a TreeSet of type Person
         //add a few
 
+        TreeSet<Person> set = new TreeSet<Person>();
+
+        set.add(new Person("Bob", 25));
+        set.add(new Person("Amy", 12));
+        set.add(new Person("Bob", 20));
+        set.add(new Person("Charlie", 35));
+        set.add(new Person("Bob", 25));
+
         //print everything
+        for (Person p : set) {
+            System.out.println(p);
+        }
     }
 }
--- monWed_sectionA/week12_april13-17/wedApril15/Person.java
+++ monWed_sectionA/week12_april13-17/wedApril15/Person.java
@@ -1,4 +1,4 @@
-public class Person {
+public class Person implements Comparable<Person> {
     private String name;
     private int age;
 
@@ -10,4 +10,30 @@ public class Person {
     public String toString() {
         return String.format("Name: %s, age: %d%n", name, age);
     }
+
+    public int compareTo(Person other) {
+        if (name.equals(other.name)) {
+            //do something with the ages
+            //if this person's age < other's age
+            if (age < other.age) {
+                //return something < 0
+                return -1;
+            }
+            else if (age > other.age) {
+                //return something > 0
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else {
+            //different names
+            return name.compareTo(other.name);
+        }
+    }
+
+    //to compare people
+    //first try to compare by name
+    //if they have the same name, then next order by age
 }
