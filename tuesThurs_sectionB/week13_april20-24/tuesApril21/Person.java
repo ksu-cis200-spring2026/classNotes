aaa4e4ec0a4ec6c541492c586bd1b23de853950c
Julie Thornton <juliet@ksu.edu>
2026-04-21 11:54:48 -0500

    gitignore

---
+++ tuesThurs_sectionB/week13_april20-24/tuesApril21/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String toString() {
+        return String.format("Name: %s, age: %d%n", name, age);
+    }
+}
