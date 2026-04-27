4546f4da8208a6e9a1947cc910388fecf13a0920
Julie <juliet@ksu.edu>
2026-04-27 15:16:44 -0500

    after mon april 27

---
+++ monWed_sectionA/week14_april27-may1/week14/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//ask for a user's age and name
+//print hello to them and their age
+
+/*
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Enter you age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+//print: Hello, John. You are 20 years old.
+Console.WriteLine($"Hello, {name}. You are {age} years old");
+*/
+
+//Bob, age 15
+Person p = new Person(){Name = "Bob", Age = 15};
+p.Age = -100;
+p.Print();
