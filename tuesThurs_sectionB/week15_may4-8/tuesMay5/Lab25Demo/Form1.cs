04b9541add994faeacb01a337a5344b33b7bb76c
Julie <juliet@ksu.edu>
2026-05-05 13:38:53 -0500

    after tues may 5

---
+++ tuesThurs_sectionB/week15_may4-8/tuesMay5/Lab25Demo/Form1.cs
@@ -0,0 +1,23 @@
+namespace Lab25Demo
+{
+    public partial class Form1 : Form
+    {
+        private int _count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uxGreet_Click(object sender, EventArgs e)
+        {
+            //this gets called whenever Greet button is clicked
+
+            _count++;
+            uxResult.Text = "You have entered " + _count + " names";
+
+            string name = uxName.Text;
+            MessageBox.Show("Hello, " + name);
+        }
+    }
+}
